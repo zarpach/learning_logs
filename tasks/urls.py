5eca88e7c429a8792cd19371ae9edbf09d11e043
Eraaly <sanazera2@gmail.com>
2024-02-22 20:35:33 +0600

    Updates

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 import tasks.views
 
 urlpatterns = [
-    path('', tasks.views.get_tasks),
-    path('<int:pk>', tasks.views.task_detail_view, name='task-detail')
+    path('', tasks.views.get_tasks, name='task-list'),
+    path('<int:pk>', tasks.views.task_detail_view, name='task-detail'),
+    path('create', tasks.views.task_create_view)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Task
 from django.views.decorators.csrf import csrf_exempt
+from .forms import TaskForm
+
 
 
 @csrf_exempt
@@ -13,10 +15,19 @@ def get_tasks(request):
     return render(request, "tasks.html", context={"tasks": tasks})
 
 
-def task_detail_view(request, id):
-    task = get_object_or_404(Task, id)
+def task_detail_view(request, pk):
+    task = Task.objects.get(id=pk)
     return render(request, 'task_detail.html', context={'task': task})
 
 
+def task_create_view(request):
+    if request.method == 'POST':
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('task-list'))
 
+    return render(request, 'task-create.html', {
+        'form': TaskForm(),
+    })
 
